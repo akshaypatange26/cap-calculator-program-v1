800c3d4cd748860e1f97f6884a73d08f289aa916
akshaypatange26 <akshaypatange26@gmail.com>
2026-07-11 15:55:06 +1000

    Update code to perform add,subtract,divide and multiply

---
+++ src/main/java/com/calculator/config/JacksonConfiguration.java
@@ -0,0 +1,17 @@
+package com.calculator.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@Configuration
+public class JacksonConfiguration {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        return mapper;
+    }
+}
